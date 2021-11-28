b94bc4aaccb7133c2764f62238c7957a55a4de99
alvince <alvince.zy@gmail.com>
2021-11-28 23:50:13 +0800

    feat: add core extensions

--- core/src/main/java/cn/alvince/zanpakuto/core/content/Intent.kt
+++ core/src/main/java/cn/alvince/zanpakuto/core/content/Intent.kt
@@ -1,11 +1,13 @@
 package cn.alvince.zanpakuto.core.content
 
 import android.app.Activity
+import android.app.Service
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.IntentSender
 import android.os.Build
+import androidx.core.content.ContextCompat
 
 inline fun Intent.createChooser(sender: IntentSender? = null, title: () -> String) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
@@ -21,4 +23,12 @@ inline fun <reified T : Activity> Context.startActivity(setup: Intent.() -> Unit
 
 inline fun Context.startActivity(component: ComponentName, setup: Intent.() -> Unit = {}) {
     startActivitySafely(Intent.makeMainActivity(component).apply(setup))
+}
+
+inline fun <reified T : Service> Context.startService(setup: Intent.() -> Unit = {}) {
+    startService(Intent(this, T::class.java).apply(setup))
+}
+
+inline fun <reified T : Service> Context.startForegroundService(setup: Intent.() -> Unit = {}) {
+    ContextCompat.startForegroundService(this, Intent(this, T::class.java).apply(setup))
 }
